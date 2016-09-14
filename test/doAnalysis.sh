95e88e2941d7d09d48627301229e9289560072fc
Kevin Pedro <kpedro88@gmail.com>
2016-09-14 11:41:21 -0500

    use 4 threads in cmssw

--- test/doAnalysis.sh
+++ test/doAnalysis.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-for name in step1_old step1_new; do
+for name in step1 step1_smear; do
   cmsRun ConfFile_standard_cfg.py inputName=${name} > log_analysis_${name}.log 2>&1 &
-  #for HcalUpgradeDataFrames in 81X
-  #cmsRun ConfFileUpg_standard_cfg.py inputName=${name} > log_analysis_${name}.log 2>&1 &
-  #for 62XSLHC
-  #cmsRun ConfFile62XSLHC_standard_cfg.py inputName=${name} > log_analysis_${name}.log 2>&1 &
 done
